b0ac40816cb3c3b28492ff8d0d3c7553c9daf3a4
kibum.lee(t14) <kissyou7@gmail.com>
2024-03-07 19:23:44 +0900

    Title: 3-2
    Signed-off-by: kibum.lee(t14) <kissyou7@gmail.com>

--- index.ts
+++ index.ts
@@ -1,3 +1,9 @@
+type User = {
+  name: string;
+  age: number;
+  married?: boolean;
+};
+
 ((chapter: string = "복습") => {
   console.log(`\n===[${chapter}]===`);
 
@@ -60,6 +66,32 @@
 
 })();
 
-((chapter: string = "3-1") => {
+((chapter: string = "3-2") => {
   console.log(`\n===[${chapter}]===`);
+
+  //Exclude는 주로 유니온 타입에서 특정 타입을 제외할 때 사용됩니다.
+  //Omit은 주로 객체 타입에서 특정 속성을 제외할 때 사용됩니다. Pick의 반대임
+
+  type myExclude<T, U> = T extends U ? never : T;
+  type myExtract<T, U> = T extends U ? T : never;
+
+  const excludeUser: myExclude<User, "married"> = {
+    name: "장동권",
+    age: 44
+  }
+
+  const extracString: myExtract<1 | "2" | 3, string> = "2"; // 이건 되는데
+  // const extractUser: myExtract<User, "name"> = { name: "이건 왜 안됨?" }; // 객체라서 안되는거임
+
+  type myOmit<T, K extends keyof any> = Pick<T, Exclude<keyof T, K>>
+
+  const omitUser: myOmit<User, "age"> = {
+    name: "홍",
+    married: true
+  }
+
+
+  type NonNullable<T> = T & {}; //{} 는 undefined, null 을 제외한 모든것
+  type nonNull = NonNullable<string | number | null>;
+
 })();
